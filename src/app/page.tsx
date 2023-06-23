f35848725b47f6153e17c70f64d878960c063074
gabriel <prates_peralta@hotmail.com>
2023-06-23 11:30:04 -0300

    feat: create Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Header } from "./components/UI/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Landing Page</h1>
+      <Header />
+      <h1>Landing Page</h1>
     </main>
-  )
+  );
 }
